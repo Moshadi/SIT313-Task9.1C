46383929ce4d94a08acf2cc704e5e8ea2fd604a5
Moshadi Hansamali <99788907+Moshadi@users.noreply.github.com>
2025-09-19 22:17:41 +1000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDXeRF6yJJ6dHqO8HHIzkMU5dV1lhbBNoM",
@@ -10,13 +9,8 @@ const firebaseConfig = {
   storageBucket: "projectweek5-df103.firebasestorage.app",
   messagingSenderId: "338824864505",
   appId: "1:338824864505:web:a74495e29cf55995e67828",
-  measurementId: "G-Z0ZJWJ9DV5"
+  measurementId: "G-Z0ZJWJ9DV5",
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth(app);
-
-
-
-
